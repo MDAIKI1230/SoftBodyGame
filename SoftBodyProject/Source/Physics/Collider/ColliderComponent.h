59b242bb90a3a791fae00132f22e96b9a4dfd2bb
MDAIKI1230 <shimamizu1230@gmail.com>
2026-06-17 05:12:49 +0900

    Suppourt純粋仮想関数追加
    ColliderComponentに追加した
    各継承したコライダーごとに関数を埋める

--- SoftBodyProject/Source/Physics/Collider/ColliderComponent.h
+++ SoftBodyProject/Source/Physics/Collider/ColliderComponent.h
@@ -5,8 +5,12 @@
 class ColliderComponent
 {
 public:
+	// ブロードフェーズ用AABBの(XYZ軸ごとの)最小値
 	const Vector3& GetBroadMin() { return aabb.min; }
+	// ブロードフェーズ用AABBの(XYZ軸ごとの)最大値
 	const Vector3& GetBroadMax() { return aabb.max; }
+	// サポート関数
+	virtual Vector3 Support(const Vector3& dir) = 0;
 protected:
 	AABBBroadPhaseCollider aabb;
 };
