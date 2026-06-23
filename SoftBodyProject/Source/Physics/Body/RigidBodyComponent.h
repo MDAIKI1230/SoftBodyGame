2e860850c58b8a40f891cbf5f2d3b3a65e27252d
MDAIKI1230 <shimamizu1230@gmail.com>
2026-06-23 13:30:06 +0900

    RigidBody変数追加
    Storageに変数分のコンテナを作ったよ。

--- SoftBodyProject/Source/Physics/Body/RigidBodyComponent.h
+++ SoftBodyProject/Source/Physics/Body/RigidBodyComponent.h
@@ -1,12 +1,9 @@
 ﻿#pragma once
 
-#include "MDMath.h"
-
 struct RigidBodyComponent
 {
 public:
 
 private:
-	// 加速度
-	Vector3 velocity;
+	int handle;
 };
--- SoftBodyProject/Source/Physics/Storage/RigidBodyComponentStorage.h
+++ SoftBodyProject/Source/Physics/Storage/RigidBodyComponentStorage.h
@@ -1,10 +1,48 @@
 ﻿#pragma once
 
+#include "MDMath.h"
+
 #include "SparseSetStorageBase.h"
 
 #include "RigidBodyComponent.h"
 
 class RigidBodyComponentStorage :public SparseSetStorageBase<RigidBodyComponent>
 {
+private:
+	// --- 速度系 ---
 
+	// 力
+	std::vector<Vector3> force;
+	// 速度
+	std::vector<Vector3> velocity;
+	// 推定移動位置
+	std::vector<Vector3> expectedPos;
+	
+	// --- 角速度系 ---
+
+	// トルク
+	std::vector<Vector3> torque;
+	// 角速度
+	std::vector<float>angularVelocity;
+	// 推定回転角度
+	std::vector<Quaternion> expectedQuat;
+
+	// --- 重力系 ---
+
+	// 重力フラグ
+	std::vector<bool> isGravity;
+	// 重力の強さと方向
+	std::vector<Vector3> garavity;
+
+	// --- 質量系 ---
+
+	// 質量
+	std::vector<float> mass;
+	// 質量の逆数
+	std::vector<float> inverseMass;
+	// 慣性テンソルの逆数
+	std::vector<Matrix4x4> inverseInertiaTensor;
+
+	// マテリアルID
+	std::vector<int> physicsMatrialID;
 };
