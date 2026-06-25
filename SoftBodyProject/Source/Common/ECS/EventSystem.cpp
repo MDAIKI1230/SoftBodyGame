c4b45891542c85d736d2bb8fe328c5c9d1a40b87
MDAIKI1230 <shimamizu1230@gmail.com>
2026-06-25 17:51:37 +0900

    EventSystem
    一旦こいつがすべてのイベント通知をしてあげる。

---
+++ SoftBodyProject/Source/Common/ECS/EventSystem.cpp
@@ -0,0 +1,26 @@
+﻿#include "EventSystem.h"
+
+void EventSystem::Update(EventManager* _eventManager, ObjectManager* _objectManager)
+{
+	// OnCollisionEnterEventの呼び出し
+	OnCollisionEnterEvent onCollisionEnterEvent;
+	while (_eventManager->Pop(onCollisionEnterEvent))
+	{
+		_objectManager->Get(onCollisionEnterEvent.a)->OnCollisionEnter();
+		_objectManager->Get(onCollisionEnterEvent.b)->OnCollisionEnter();
+	}
+	// OnCollisionEventの呼び出し
+	OnCollisionEvent onCollisionEvent;
+	while (_eventManager->Pop(onCollisionEvent))
+	{
+		_objectManager->Get(onCollisionEvent.a)->OnCollisionEnter();
+		_objectManager->Get(onCollisionEvent.b)->OnCollisionEnter();
+	}
+	// OnCollisionExitEventの呼び出し
+	OnCollisionExitEvent onCollisionExitEvent;
+	while (_eventManager->Pop(onCollisionExitEvent))
+	{
+		_objectManager->Get(onCollisionExitEvent.a)->OnCollisionEnter();
+		_objectManager->Get(onCollisionExitEvent.b)->OnCollisionEnter();
+	}
+}
---
+++ SoftBodyProject/Source/Common/ECS/EventSystem.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "EventManager.h"
+#include "ObjectManager.h"
+
+class EventSystem
+{
+public:
+	void Update(EventManager* _eventManager, ObjectManager* _objectManager);
+};
