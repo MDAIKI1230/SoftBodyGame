9f22aa3c74b8bf458b6d3e41ef2d584b34666800
MDAIKI1230 <shimamizu1230@gmail.com>
2026-06-25 18:51:32 +0900

    Buffer系追加

---
+++ SoftBodyProject/Source/Physics/Storage/CollisionContactStorage.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "MDMath.h"
+
+#include "BufferBase.h"
+
+class CollisionContactBuffer :public BufferBase
+{
+public:
+	// 衝突したやつA
+	std::vector<int> handleA;
+	// 衝突したやつB
+	std::vector<int> handleB;
+	// 深度
+	std::vector<float> depth;
+	// 法線
+	std::vector<Vector3> normal;
+};
