2d12a5c3b7e3cf96dbfa9289d21773922591eee5
MDAIKI1230 <shimamizu1230@gmail.com>
2026-06-25 22:38:25 +0900

    CollisionSolverSystem回転系以外埋めた

--- SoftBodyProject/Source/Physics/System/Collision/CollisionSolverSystem.cpp
+++ SoftBodyProject/Source/Physics/System/Collision/CollisionSolverSystem.cpp
@@ -2,25 +2,63 @@
 
 void CollisionSolverSystem::FixedUpdate(WorldStorage* _worldStorage, CollisionManifoldBuffer* _manifoldBuffer)
 {
+	RigidBodyComponentStorage* bodyStorage{ static_cast<RigidBodyComponentStorage*>(_worldStorage->GetStorage<RigidBodyComponent>()) };
 
+	PositionSolver(bodyStorage, _manifoldBuffer);
+	VelocitySolver(bodyStorage, _manifoldBuffer);
+	OrientationSolver(bodyStorage, _manifoldBuffer);
+	RotationSolver(bodyStorage, _manifoldBuffer);
 }
 
-void CollisionSolverSystem::PositionSolver()
+void CollisionSolverSystem::PositionSolver(RigidBodyComponentStorage* _bodyStorage, CollisionManifoldBuffer* _manifoldBuffer)
+{
+	int size{ _manifoldBuffer->manifolds.size() };
+
+	for (int i{ size }; i < size; i++)
+	{
+		Manifold& manifold{ _manifoldBuffer->manifolds[i] };
+		Vector3 vec{ manifold.normal * manifold.points[0].penetration };
+
+		_bodyStorage->expectedPos[manifold.handleA] -= vec;
+		_bodyStorage->expectedPos[manifold.handleB] -= -vec;
+	}
+}
+
+void CollisionSolverSystem::VelocitySolver(RigidBodyComponentStorage* _bodyStorage, CollisionManifoldBuffer* _manifoldBuffer)
+{
+	int size{ _manifoldBuffer->manifolds.size() };
+
+	for (int i{ size }; i < size; i++)
+	{
+		Manifold& manifold{ _manifoldBuffer->manifolds[i] };
+		int a{ manifold.handleA };
+		int b{ manifold.handleB };
+
+		// どちらかRigidBodyがないならaが-1になる。
+		if(a != -1)
+		{
+			Vector3 relativeVec{ _bodyStorage->velocity[b] - _bodyStorage->velocity[a] };
+			float massCoefficient{ (_bodyStorage->mass[a] * _bodyStorage->mass[b]) / (_bodyStorage->mass[a] + _bodyStorage->mass[b]) };
+			float j{ massCoefficient * Vector3::Dot(relativeVec, manifold.normal) };
+			_bodyStorage->velocity[a] + (manifold.normal * j) / _bodyStorage->mass[a];
+			_bodyStorage->velocity[b] - (manifold.normal * j) / _bodyStorage->mass[b];
+		}
+		else
+		{
+			// 質量は1と仮定
+			float massCoefficient{ (_bodyStorage->mass[b]) / (_bodyStorage->mass[b] + 1) };
+			float j{ massCoefficient * Vector3::Dot(_bodyStorage->velocity[b], manifold.normal) };
+			_bodyStorage->velocity[b] + (manifold.normal * j) / _bodyStorage->mass[b];
+		}
+	}
+}
+
+void CollisionSolverSystem::OrientationSolver(RigidBodyComponentStorage* _bodyStorage, CollisionManifoldBuffer* _manifoldBuffer)
 {
 
 }
 
-void CollisionSolverSystem::VelocitySolver()
-{
-
-}
-
-void CollisionSolverSystem::OrientationSolver()
-{
-
-}
-
-void CollisionSolverSystem::RotationSolver()
+void CollisionSolverSystem::RotationSolver(RigidBodyComponentStorage* _bodyStorage, CollisionManifoldBuffer* _manifoldBuffer)
 {
 
 }
--- SoftBodyProject/Source/Physics/System/Collision/CollisionSolverSystem.h
+++ SoftBodyProject/Source/Physics/System/Collision/CollisionSolverSystem.h
@@ -3,17 +3,19 @@
 #include "WorldStorage.h"
 #include "CollisionManifoldBuffer.h"
 
+#include "RigidBodyComponentStorage.h"
+
 class CollisionSolverSystem
 {
 public:
 	void FixedUpdate(WorldStorage* _worldStorage, CollisionManifoldBuffer* _manifoldBuffer);
 private:
 	// 位置解決
-	void PositionSolver();
+	void PositionSolver(RigidBodyComponentStorage* _bodyStorage);
 	// 速度解決
-	void VelocitySolver();
+	void VelocitySolver(RigidBodyComponentStorage* _bodyStorage);
 	// 姿勢解決
-	void OrientationSolver();
+	void OrientationSolver(RigidBodyComponentStorage* _bodyStorage);
 	// 回転速度解決
-	void RotationSolver();
+	void RotationSolver(RigidBodyComponentStorage* _bodyStorage);
 };
