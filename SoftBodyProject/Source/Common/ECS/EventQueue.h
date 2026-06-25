3c703c85960b04bbb2f739ccdf8f469a11431170
MDAIKI1230 <shimamizu1230@gmail.com>
2026-06-25 16:06:16 +0900

    イベントキュー作成

---
+++ SoftBodyProject/Source/Common/ECS/EventQueue.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include <queue>
+
+template<typename T>
+class EventQueue
+{
+public:
+	// 追加
+	void Push(const T& _event)
+	{
+		write.push(_event);
+	}
+
+	// 取り出し
+	bool Pop(T& out)
+	{
+		if (read.empty())
+			return false;
+
+		out = read.front();
+		read.pop();
+		return true;
+	}
+
+	// 空チェック
+	bool IsEmpty()
+	{
+		return read.empty();
+	}
+
+	// スワップ
+	void Swap()
+	{
+		std::swap(write, read);
+
+		std::queue<T> empty;
+		std::swap(write, empty);
+	}
+
+private:
+	std::queue<T> read;
+	std::queue<T> write;
+};
---
+++ SoftBodyProject/Source/Common/Events/Physics/PhysicsEvent.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+struct OnCollisionEnterEvent
+{
+	int a, b;
+};
+
+struct OnCollisionEvent
+{
+	int a, b;
+};
+
+struct OnCollisionExitEvent
+{
+	int a, b;
+};
