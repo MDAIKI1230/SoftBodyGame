625a8acf6a6404eca867267f9a28220391d6c10f
MDAIKI1230 <shimamizu1230@gmail.com>
2026-06-26 15:37:04 +0900

    ハンドル取得関数追加

--- SoftBodyProject/Source/Common/ECS/SparseSetStorageBase.h
+++ SoftBodyProject/Source/Common/ECS/SparseSetStorageBase.h
@@ -52,6 +52,8 @@ public:
 	virtual std::vector<T>* GetDense() { return sparseSet.GetDense(); }
 	// エンティティコンテナ取得
 	virtual std::vector<int>* GetEntities() { return sparseSet.GetEntities(); }
+	// ハンドル取得
+	virtual int GetHandle() { return sparseSet.GetSize(); }
 
 	// 仮想デストラクタ
 	virtual ~SparseSetStorageBase() = default;
