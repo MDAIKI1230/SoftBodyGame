7d1c12f1b160ce43724b8855711e5b5af8d4cc11
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-08 02:13:30 +0900

    Constraint構造体整理。

--- SoftBodyProject/Source/Physics/Struct/Constraint/Constraint.h
+++ SoftBodyProject/Source/Physics/Struct/Constraint/Constraint.h
@@ -4,6 +4,17 @@
 
 struct Constraint
 {
+	// ソルバボディA
+	uint32_t solverBodyAIndex;
+	// ソルバボディB
+	uint32_t solverBodyBIndex;
+
+	// ズレの数値(拘束条件Cの値と考えてOK)
 	float constraintError;
-	Vector3 jacobian;
+	// Aの速度、Aの角速度、Bの速度、Bの角速度
+	Vector3 jacobian[4];
+
+	// 蓄積したλ
+	float accumulatedLambda{ 0 };
+	float accumulatedFrictionLambda{ 0 };
 };
