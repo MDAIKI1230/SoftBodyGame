d0e6956b558fb03b620f23732026c28ef9bd7a40
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-08 15:07:40 +0900

    Constraintを解くための準備
    SolverBodyBuffer作成
    ConsraintSolverSystem追加

---
+++ SoftBodyProject/Source/Physics/Storage/SolverBodyBuffer.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <vector>
+#include <unordered_map>
+
+#include "SolverBody.h"
+
+class SolverBodyBuffer
+{
+public:
+	void AddSolverBody(PhysicsTransformID& _transformID, SolverBody& _body)
+	{
+		bodyMap[_transformID] = solverBodies.size();
+		solverBodies.push_back(_body);
+	}
+public:
+	std::vector<SolverBody> solverBodies;
+	std::unordered_map<PhysicsTransformID, uint32_t> bodyMap;
+};
