7adcd615251e6cc6b2f2ccf03ef3ede64293a79d
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-08 10:02:51 +0900

    ConstraintID、PointConstraintComponentの構造体追加

---
+++ SoftBodyProject/Source/Physics/Component/Constraint/PointConstraintComponent.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "ComponentBase.h"
+
+#include "MDMath.h"
+
+#include "EntityID.h"
+
+#include "ConstraintID.h"
+
+struct PointConstraintComponent : public ComponentBase
+{
+public:
+	// コンストラクタ
+	PointConstraintComponent(int _handle) :
+		ComponentBase{ _handle }
+	{
+	}
+
+	void AddEndPoint(EntityID& _entityID, Vector3& _localOffset);
+private:
+	ConstraintID id;
+};
