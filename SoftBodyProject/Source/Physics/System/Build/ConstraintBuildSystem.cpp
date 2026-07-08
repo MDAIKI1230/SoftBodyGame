1457e60836d0e74110172646215f20310cba0a72
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-08 14:29:37 +0900

    ConstraintBuildSystem置いといた

---
+++ SoftBodyProject/Source/Physics/System/Build/ConstraintBuildSystem.cpp
@@ -0,0 +1,14 @@
+﻿#include "ConstraintBuildSystem.h"
+
+void ConstraintBuildSystem::FixedUpdate(PhysicsTransformStorage* _transformStorage, ConstraintStorage* _constraintStorage, ConstraintBuffer* _constraintBuffer)
+{
+	BuildPointConstraint(_transformStorage, _constraintStorage, _constraintBuffer);
+}
+
+void ConstraintBuildSystem::BuildPointConstraint(PhysicsTransformStorage* _transformStorage, ConstraintStorage* _constraintStorage, ConstraintBuffer* _constraintBuffer)
+{
+	for (auto& constraint : _constraintStorage->pointConstraintStorage->constraints)
+	{
+		
+	}
+}
---
+++ SoftBodyProject/Source/Physics/System/Build/ConstraintBuildSystem.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "ConstraintStorage.h"
+#include "PhysicsTransformStorage.h"
+
+#include "ConstraintBuffer.h"
+
+class ConstraintBuildSystem
+{
+public:
+	void FixedUpdate(PhysicsTransformStorage* _transformStorage, ConstraintStorage* _constraintStorage, ConstraintBuffer* _constraintBuffer);
+private:
+	void BuildPointConstraint(PhysicsTransformStorage* _transformStorage, ConstraintStorage* _constraintStorage, ConstraintBuffer* _constraintBuffer);
+};
