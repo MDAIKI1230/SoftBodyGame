f7f57b5d50ab0cee42adcc1615506b7d74c485b0
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-12 23:42:36 +0900

    距離拘束を拘束構造体に詰めるぞ(仮)

--- SoftBodyProject/Source/Physics/System/Build/ConstraintBuildSystem.cpp
+++ SoftBodyProject/Source/Physics/System/Build/ConstraintBuildSystem.cpp
@@ -3,6 +3,7 @@
 void ConstraintBuildSystem::FixedUpdate(ConstraintStorage* _constraintStorage, SolverBodyBuffer* _solverBodyBuffer, ConstraintBuffer* _constraintBuffer)
 {
 	BuildPointConstraint(_constraintStorage, _solverBodyBuffer, _constraintBuffer);
+	BuildDistanceConstraint(_constraintStorage, _solverBodyBuffer, _constraintBuffer);
 }
 
 void ConstraintBuildSystem::BuildPointConstraint(ConstraintStorage* _constraintStorage, SolverBodyBuffer* _solverBodyBuffer, ConstraintBuffer* _constraintBuffer)
@@ -19,6 +20,7 @@ void ConstraintBuildSystem::BuildPointConstraint(ConstraintStorage* _constraintS
 		{
 			continue;
 		}
+
 		// 基準点となるボディから位置を持ってくる。
 		uint32_t basePointIndex{ _solverBodyBuffer->bodyMap[pointConstraint.endPoints[0].transformID] };
 		SolverBody& solverBodyBase{ _solverBodyBuffer->solverBodies[basePointIndex] };
@@ -76,6 +78,59 @@ void ConstraintBuildSystem::BuildPointConstraint(ConstraintStorage* _constraintS
 			constraint.jacobian[1] = Vector3::Cross(rA, Vector3::FORWARD);
 			constraint.jacobian[2] = -Vector3::FORWARD;
 			constraint.jacobian[3] = -Vector3::Cross(rB, Vector3::FORWARD);
+
+			// 拘束として追加
+			_constraintBuffer->constraints.push_back(constraint);
+		}
+	}
+}
+
+void ConstraintBuildSystem::BuildDistanceConstraint(ConstraintStorage* _constraintStorage, SolverBodyBuffer* _solverBodyBuffer, ConstraintBuffer* _constraintBuffer)
+{
+	// 拘束が存在するかチェック
+	if (_constraintStorage->distanceConstraintStorage->constraints.size() <= 0)
+	{
+		return;
+	}
+	for (auto& distanceConstraint : _constraintStorage->distanceConstraintStorage->constraints)
+	{
+		// ポイントが2つ以上じゃないと拘束なんて発生しない
+		if (distanceConstraint.endPoints.size() <= 1)
+		{
+			continue;
+		}
+
+		// 基準点となるボディから位置を持ってくる。
+		uint32_t basePointIndex{ _solverBodyBuffer->bodyMap[distanceConstraint.endPoints[0].transformID] };
+		SolverBody& solverBodyBase{ _solverBodyBuffer->solverBodies[basePointIndex] };
+		Vector3 basePoint{ solverBodyBase.position + solverBodyBase.rotation.Rotate(distanceConstraint.endPoints[0].localPoint) };
+
+		for (int i{ 1 }; i < distanceConstraint.endPoints.size(); i++)
+		{
+			Constraint constraint;
+
+			// 対象の位置を取得
+			uint32_t pointIndex{ _solverBodyBuffer->bodyMap[distanceConstraint.endPoints[i].transformID] };
+			SolverBody solverBody{ _solverBodyBuffer->solverBodies[pointIndex] };
+			Vector3 point{ solverBody.position + solverBody.rotation.Rotate(distanceConstraint.endPoints[i].localPoint) };
+
+			constraint.solverBodyAIndex = basePointIndex;
+			constraint.solverBodyBIndex = pointIndex;
+
+			Vector3 rA = basePoint - solverBodyBase.position;
+			Vector3	rB = point - solverBody.position;
+
+			// 差
+			Vector3 diff{ basePoint - point };
+			// 差をそのまま拘束Cの結果とする
+			constraint.constraintError = diff.Length() - distanceConstraint.distance;
+
+			Vector3 normal{ diff.Normalized() };
+			// ヤコビアンの計算
+			constraint.jacobian[0] = normal;
+			constraint.jacobian[1] = Vector3::Cross(rA, normal);
+			constraint.jacobian[2] = -normal;
+			constraint.jacobian[3] = -Vector3::Cross(rB, normal);
 
 			// 拘束として追加
 			_constraintBuffer->constraints.push_back(constraint);
--- SoftBodyProject/Source/Physics/System/Build/ConstraintBuildSystem.h
+++ SoftBodyProject/Source/Physics/System/Build/ConstraintBuildSystem.h
@@ -11,4 +11,5 @@ public:
 	void FixedUpdate(ConstraintStorage* _constraintStorage, SolverBodyBuffer* _solverBodyBuffer, ConstraintBuffer* _constraintBuffer);
 private:
 	void BuildPointConstraint(ConstraintStorage* _constraintStorage, SolverBodyBuffer* _solverBodyBuffer, ConstraintBuffer* _constraintBuffer);
+	void BuildDistanceConstraint(ConstraintStorage* _constraintStorage, SolverBodyBuffer* _solverBodyBuffer, ConstraintBuffer* _constraintBuffer);
 };
