c00f4a2f3b8c17c6ac6f9126aff3868162387a77
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-12 22:43:20 +0900

    距離拘束のストレージ追加

---
+++ SoftBodyProject/Source/Physics/Storage/Constraint/DistanceConstraintStorage.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "DistanceConstraint.h"
+#include "ConstraintID.h"
+
+class DistanceConstraintStorage
+{
+public:
+    ConstraintID Remove(uint32_t _index)
+    {
+        if (id.empty())
+        {
+            return ConstraintID{ 0, 0 };
+        }
+
+        size_t last = id.size() - 1;
+        ConstraintID movedId = id[last];
+
+        if (_index != last)
+        {
+            constraints[_index] = constraints[last];
+            id[_index] = id[last];
+        }
+
+        constraints.pop_back();
+        id.pop_back();
+
+        return movedId;
+    }
+public:
+    std::vector<ConstraintID> id;
+    std::vector<DistanceConstraint> constraints;
+};
