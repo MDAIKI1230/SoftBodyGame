9bfb9701eb4875aa0e15a99865d7379c60f27fb5
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-12 22:37:55 +0900

    距離拘束のコンポーネント追加

---
+++ SoftBodyProject/Source/Physics/Component/Constraint/DistanceConstraintComponent.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "ComponentBase.h"
+
+#include "MDMath.h"
+
+#include "EntityID.h"
+
+#include "ConstraintID.h"
+
+struct DistanceConstraintComponent : public ComponentBase
+{
+public:
+	// コンストラクタ
+	DistanceConstraintComponent(EntityID _entity, int _handle);
+	// コンストラクタ
+	DistanceConstraintComponent(EntityID _entity, int _handle, Vector3 _localOffset);
+	// コンストラクタ
+	DistanceConstraintComponent(EntityID _entity, int _handle, float _distance);
+	// コンストラクタ
+	DistanceConstraintComponent(EntityID _entity, int _handle, Vector3 _localOffset, float _distance);
+
+	// 対応点追加
+	void AddEndPoint(const EntityID& _entityID, const Vector3& _localOffset);
+
+	// 距離
+	float SetDistance(float _distance);
+private:
+	ConstraintID id;
+};
