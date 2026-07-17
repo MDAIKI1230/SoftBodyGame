1e6af9bdfe0f606cee2ece3c219aae7ffee1fee8
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-17 18:10:13 +0900

    コマンドバッファ一旦置いとく

---
+++ SoftBodyProject/Source/Physics/Storage/Buffer/PhysicsCommandBuffer.cpp
@@ -0,0 +1,12 @@
+﻿#include "PhysicsCommandBuffer.h"
+
+void PhysicsCommandBuffer::BeginRecord()
+{
+	currentStamp++;
+	dirtyIndices.clear();
+}
+
+void PhysicsCommandBuffer::SecureCapacity(uint32_t _size)
+{
+	stamps.reserve(_size);
+}
--- SoftBodyProject/Source/Physics/Storage/Buffer/PhysicsCommandBuffer.h
+++ SoftBodyProject/Source/Physics/Storage/Buffer/PhysicsCommandBuffer.h
@@ -1,6 +1,18 @@
 ﻿#pragma once
 
+#include <vector>
+
 class PhysicsCommandBuffer
 {
+public:
+	// 記録開始
+	void BeginRecord();
+private:
+	// メモリ確保
+	void SecureCapacity(uint32_t _size);
+private:
+	uint32_t currentStamp{ 0 };
 
+	std::vector<uint32_t> stamps;
+	std::vector<uint32_t> dirtyIndices;
 };
