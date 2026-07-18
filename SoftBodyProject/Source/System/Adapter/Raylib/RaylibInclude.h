469ec86c4a7e27435fe4dfdf4e573733e0f24306
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-18 22:46:49 +0900

    Raylib追加(LoadDivGraph以外)

---
+++ SoftBodyProject/Source/System/Adapter/Raylib/RaylibInclude.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#define Vector2    RlVector2
+#define Vector3    RlVector3
+#define Vector4    RlVector4
+#define Quaternion RlQuaternion
+#define Matrix     RlMatrix
+#define Color      RlColor
+#define Camera     RlCamera
+#define Camera2D   RlCamera2D
+#define Camera3D   RlCamera3D
+#define Transform  RlTransform
+
+using RlModel = ::Model;
+using RlTexture2D = ::Texture2D;
+
+#include <raylib.h>
+#include <rlgl.h>
+
+#undef Vector2
+#undef Vector3
+#undef Vector4
+#undef Quaternion
+#undef Matrix
+#undef Color
+#undef Camera
+#undef Camera2D
+#undef Camera3D
+#undef Transform
---
+++ SoftBodyProject/Source/System/Adapter/Raylib/RaylibRenderer.h
@@ -0,0 +1,83 @@
+﻿#include <unordered_map>
+
+#include "IRenderer.h"
+
+#include "RaylibConvert.h"
+
+class RaylibRenderer :public IRenderer
+{
+public:
+	// カメラ関連
+	// カメラセット
+	virtual void SetCamera(const Camera& _camera) = 0;
+	// ---ダブルバッファリング用---
+
+	// 垂直同期信号を待つ
+	virtual int ClearDrawScreen() = 0;
+	// ダブルバッファリングの反転
+	virtual int ScreenFlip() = 0;
+	// --- 設定 ---
+
+	// ZDepth使う
+	virtual int SetUseZDepth(bool _flag) = 0;
+	// ZDepth書き込み
+	virtual int SetWriteZDepth(bool _flag) = 0;
+
+	// ---読み込み関数---
+
+	// モデルの読み込み
+	virtual int LoadModel(const std::string& _fileName) = 0;
+	// 画像の読み込み
+	virtual int LoadGraph(const std::string& _fileName) = 0;
+	/// <summary>
+	/// 画像の分割読み込み
+	/// </summary>
+	/// <param name="_fileName">ファイル名</param>
+	/// <param name="_allNum">分割全体数</param>
+	/// <param name="_xNum">横の数</param>
+	/// <param name="_yNum">縦の数</param>
+	/// <param name="_xSize">分割した一つの横幅</param>
+	/// <param name="_ySize">分割した一つの縦幅</param>
+	/// <param name="handleBuf">配列のアドレス</param>
+	virtual void LoadDivGraph(const std::string& _fileName, int _allNum, int _xNum, int _yNum, int _xSize, int _ySize, int* _handleBuf) = 0;
+	// モデル情報セット系
+	// 行列セット
+	virtual void ModelSetMatrix(int _handle, const Matrix4x4& _mat) = 0;
+	// ---描画関数---
+
+	// モデル描画
+	virtual void DrawModel(int _handle) = 0;
+	// 画像描画
+	virtual void DrawGraph(const Vector2& _pos, int _handle, bool _transFlag) = 0;
+	// 球描画
+	virtual void DrawSphere(const Vector3& _pos, float _radius, const Color& _color) = 0;
+	// Box描画
+	virtual void DrawBox(const Matrix4x4& _mat, const Vector3& _size, const Color& _color) = 0;
+	// 線描画
+	virtual void DrawLine(const Vector3& _pos1, const Vector3& _pos2, const Color& _color) = 0;
+	// ---リソース削除関数---
+
+	// モデル素材削除
+	virtual void DeleteModel(int _handle) = 0;
+	// 画像素材削除
+	virtual void DeleteGraph(int _handle) = 0;
+	// デストラクタ
+	~RaylibRenderer() = default;
+
+private:
+	bool useZDepth{ true };
+	bool writeZDepth{ true };
+
+	int nextResourceHandle{ 1 };
+
+	std::unordered_map<int, RlModel> modelMap;
+	std::unordered_map<int, RlTexture2D> textureMap;
+
+	RlCamera3D raylibCamera{
+		RlVector3{0.0f, 0.0f, -10.0f}, // position
+		RlVector3{0.0f, 0.0f,  0.0f},  // target
+		RlVector3{0.0f, 1.0f,  0.0f},  // up
+		45.0f,                         // vertical FOV
+		CAMERA_PERSPECTIVE
+	};
+};
