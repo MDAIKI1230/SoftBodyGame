7d450ec3ccd651c6fdb1b9adfee9919e3afd2bd6
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-23 10:15:39 +0900

    クロスコンポーネントストレージ追加

---
+++ SoftBodyProject/Source/Physics/Storage/Component/Body/Soft/ClothComponentStorage.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "SparseSetStorageBase.h"
+
+#include "ClothComponent.h"
+
+
+class ClothComponentStorage :public SparseSetStorageBase<ClothComponent>
+{
+private:
+	bool CanAdd(EntityID& _entity) override;
+};
