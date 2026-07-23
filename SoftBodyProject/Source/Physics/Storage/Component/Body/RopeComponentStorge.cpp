35dd5e64b8f5e655961c28e7822ee1aa5a638e66
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-23 10:12:06 +0900

    ロープコンポーネントストレージ追加

---
+++ SoftBodyProject/Source/Physics/Storage/Component/Body/RopeComponentStorge.cpp
@@ -0,0 +1,8 @@
+﻿#include "PhysicsAPI.h"
+
+#include "RopeComponentStorge.h"
+
+bool RopeComponentStorge::CanAdd(EntityID& _entity)
+{
+	return PhysicsAPI::CanAddBody(_entity);
+}
---
+++ SoftBodyProject/Source/Physics/Storage/Component/Body/RopeComponentStorge.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "SparseSetStorageBase.h"
+
+#include "RopeComponent.h"
+
+
+class RopeComponentStorge:public SparseSetStorageBase<RopeComponent>
+{
+private:
+	bool CanAdd(EntityID& _entity) override;
+};
