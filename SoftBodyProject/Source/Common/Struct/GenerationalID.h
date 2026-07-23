db5c3108bd3438ae8300705814e53b48800bf01a
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-23 12:59:56 +0900

    無効IDの追加

--- SoftBodyProject/Source/Common/Struct/GenerationalID.h
+++ SoftBodyProject/Source/Common/Struct/GenerationalID.h
@@ -11,6 +11,8 @@ struct GenerationalID
 	using Generation = uint32_t;
 public:
 	// コンストラクタ
+	constexpr GenerationalID() = default;
+	// コンストラクタ
 	GenerationalID(Index _index, Generation _generation) :
 		index{ _index },
 		generation{ _generation }
@@ -22,8 +24,16 @@ public:
 	// Generation取得
 	Generation GetGeneration() const { return generation; }
 
+	// 無効なIDじゃないか確認関数
+	constexpr bool IsValid() const
+	{
+		return generation_ != INVALID_GENERATION;
+	}
+
 	// 全部の変数比較
 	bool operator==(const GenerationalID&) const = default;
+public:
+	static constexpr Generation INVALID_GENERATION{ 0 };
 private:
 	Index index{ 0 };
 	Generation generation{ 0 };
