781ca892343a44f2b5c408f1214e6bd892f7b9e4
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-25 04:09:00 +0900

    マクロの命名を直したり

--- SoftBodyProject/Source/Common/Macro/StorageAccessorsMacros.h
+++ SoftBodyProject/Source/Common/Macro/StorageAccessorsMacros.h
@@ -93,7 +93,7 @@ namespace MD::Storage::Detail
 	}
 }
 
-/// @def MD_STORAGE_READ_WRITE_COLUMN(Type, _name, _member)
+/// @def MD_STORAGE_READ_WRITE_COLUMN(Type, _tag, _member)
 /// @brief 読み書き可能なStorage Columnを用意します。
 ///
 /// 次の関数を生成します。
@@ -106,51 +106,51 @@ namespace MD::Storage::Detail
 /// - EmptyName
 ///
 /// @param Type 格納する要素型
-/// @param _name 関数名に使用する名前
+/// @param _tag 変数が何か(関数名に使用される)
 /// @param _member 内部メンバー変数名
 ///
 /// @warning 取得した参照やRangeは再確保後に無効になります。
-#define MD_STORAGE_READ_WRITE_COLUMN(Type, _name, _member)\
+#define MD_STORAGE_READ_WRITE_COLUMN(Type, _tag, _member)\
 public:\
 	/* 値読み取り */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_name(uint32_t _index) const\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_tag(uint32_t _index) const\
 	{\
 		return MD::Storage::Detail::Get(_member, _index);\
 	}\
 	/* 値参照渡しで変更できる */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Edit##_name(uint32_t _index)\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Edit##_tag(uint32_t _index)\
 	{\
 		return MD::Storage::Detail::Edit(_member, _index);\
 	}\
 	/* 値代入 */\
-	void Set##_name(uint32_t _index, const Type& _value)\
+	void Set##_tag(uint32_t _index, const Type& _value)\
 	{\
 		MD::Storage::Detail::Set(_member, _index, _value);\
 	}\
 	/* 配列の要素数 */\
-	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_tag() const noexcept\
 	{\
 		return MD::Storage::Detail::Count(_member);\
 	}\
 	/* 空チェック */\
-	[[nodiscard("戻り値が無視されています")]] bool Empty##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] bool Empty##_tag() const noexcept\
 	{\
 		return MD::Storage::Detail::Empty(_member);\
 	}\
 	/* 変化不可配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Get##_name##Range() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Get##_tag##Range() const noexcept\
 	{\
 		return MD::Storage::Detail::GetRange(_member);\
 	}\
 	/* 変化可能配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Edit##_name##Range() noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Edit##_tag##Range() noexcept\
 	{\
 		return MD::Storage::Detail::EditRange(_member);\
 	}\
 private:\
 	std::vector<Type> _member;
 
-/// @def MD_STORAGE_READ_ONLY_COLUMN(Type, _name, _member)
+/// @def MD_STORAGE_READ_ONLY_COLUMN(Type, _tag, _member)
 /// @brief 読み込み可能なStorage Columnを用意します。
 ///
 /// 次の関数を生成します。
@@ -160,36 +160,36 @@ private:\
 /// - EmptyName
 ///
 /// @param Type 格納する要素型
-/// @param _name 関数名に使用する名前
+/// @param _tag 変数が何か(関数名に使用される)
 /// @param _member 内部メンバー変数名
 ///
 /// @warning 取得した参照やRangeは再確保後に無効になります。
-#define MD_STORAGE_READ_ONLY_COLUMN(Type, _name, _member)\
+#define MD_STORAGE_READ_ONLY_COLUMN(Type, _tag, _member)\
 public:\
 	/* 値読み取り */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_name(uint32_t _index) const\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_tag(uint32_t _index) const\
 	{\
 		return MD::Storage::Detail::Get(_member, _index);\
 	}\
 	/* 配列の要素数 */\
-	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_name() const\
+	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_tag() const\
 	{\
 		return MD::Storage::Detail::Count(_member);\
 	}\
 	/* 空チェック */\
-	[[nodiscard("戻り値が無視されています")]] bool Empty##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] bool Empty##_tag() const noexcept\
 	{\
 		return MD::Storage::Detail::Empty(_member);\
 	}\
 	/* 変化不可配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Get##_name##Range() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Get##_tag##Range() const noexcept\
 	{\
 		return MD::Storage::Detail::GetRange(_member);\
 	}\
 private:\
 	std::vector<Type> _member;
 
-/// @def MD_STORAGE_WRITE_ORIGINAL_COLUMN(Type, _name, _member)
+/// @def MD_STORAGE_WRITE_ORIGINAL_COLUMN(Type, _tag, _member)
 /// @brief 独自にSetterを作成可能なStorage Columnを用意します。
 ///
 /// 次の関数を生成します。
@@ -199,14 +199,14 @@ private:\
 /// - EmptyName
 ///
 /// @param Type 格納する要素型
-/// @param _name 関数名に使用する名前
+/// @param _tag 変数が何か(関数名に使用される)
 /// @param _member 内部メンバー変数名
 ///
 /// @warning 取得した参照やRangeは再確保後に無効になります。
-#define MD_STORAGE_WRITE_ORIGINAL_COLUMN(Type, _name, _member)\
-	MD_STORAGE_READ_ONLY_COLUMN(Type, _name, _member);
+#define MD_STORAGE_WRITE_ORIGINAL_COLUMN(Type, _tag, _member)\
+	MD_STORAGE_READ_ONLY_COLUMN(Type, _tag, _member);
 
-/// @def MD_STORAGE_BUFFER_COLUMN(Type, _name, _member)
+/// @def MD_STORAGE_BUFFER_COLUMN(Type, _tag, _member)
 /// @brief Buffer用変数の追加
 ///
 /// 次の関数を生成します。
@@ -219,71 +219,71 @@ private:\
 /// - EmptyName
 ///
 /// @param Type 格納する要素型
-/// @param _name 関数名に使用する名前
+/// @param _tag 変数が何か(関数名に使用される)
 /// @param _member 内部メンバー変数名
 ///
 /// @warning 取得した参照やRangeは再確保後に無効になります。
-#define MD_STORAGE_BUFFER_COLUMN(Type, _name, _member)\
+#define MD_STORAGE_BUFFER_COLUMN(Type, _tag, _member)\
 public:\
 	/* 値読み取り */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_name(uint32_t _index) const\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_tag(uint32_t _index) const\
 	{\
 		return MD::Storage::Detail::Get(_member, _index);\
 	}\
 	/* 値参照渡しで変更できる */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Edit##_name(uint32_t _index)\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Edit##_tag(uint32_t _index)\
 	{\
 		return MD::Storage::Detail::Edit(_member, _index);\
 	}\
 	/* 値代入 */\
-	void Set##_name(uint32_t _index, const Type& _value)\
+	void Set##_tag(uint32_t _index, const Type& _value)\
 	{\
 		MD::Storage::Detail::Set(_member, _index, _value);\
 	}\
 	/* 配列の要素数 */\
-	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_tag() const noexcept\
 	{\
 		return MD::Storage::Detail::Count(_member);\
 	}\
 	/* 空チェック */\
-	[[nodiscard("戻り値が無視されています")]] bool Empty##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] bool Empty##_tag() const noexcept\
 	{\
 		return MD::Storage::Detail::Empty(_member);\
 	}\
 	/* 変化不可配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Get##_name##Range() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Get##_tag##Range() const noexcept\
 	{\
 		return MD::Storage::Detail::GetRange(_member);\
 	}\
 	/* 変化可能配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Edit##_name##Range() noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Edit##_tag##Range() noexcept\
 	{\
 		return MD::Storage::Detail::EditRange(_member);\
 	}\
 	/* 追加生成 */\
 	template<class... Args>\
-	void Emplace##_name(Args&&... _args)\
+	void Emplace##_tag(Args&&... _args)\
 	{\
 		MD::Storage::Detail::Emplace(_member,　std::forward<Args>(_args)...);\
 	/* 追加 */\
-	void Add##_name(const Type& _value)\
+	void Add##_tag(const Type& _value)\
 	{\
 		MD::Storage::Detail::Add(_member, _value);\
 	}\
 	/* メモリ確保 */\
-	void Reserve##_name(uint32_t _size)\
+	void Reserve##_tag(uint32_t _size)\
 	{\
 		MD::Storage::Detail::Reserve(_member, _size);\
 	}\
 	/* 全要素削除 */\
-	void Clear##_name() noexcept\
+	void Clear##_tag() noexcept\
 	{\
 		MD::Storage::Detail::Clear(_member);\
 	}\
 private:\
 	std::vector<Type> _member;
 
-/// @def MD_OWNED_STORAGE_READ_WRITE_ACCESSORS(IDType, _name, _storageName, _member)
+/// @def MD_OWNED_STORAGE_READ_WRITE_ACCESSORS(IDType, _tag, _storageName, _member, _memberTag)
 /// @brief 読み書き可能な子ストレージのラッパー関数を用意します。
 ///
 /// 次の関数を生成します。
@@ -297,50 +297,51 @@ private:\
 ///
 /// @param IDType 対応するIDの型
 /// @param MemberType 変数の型
-/// @param _name 関数名に使用する名前
-/// @param _storageName ストレージの変数
+/// @param _tag 変数が何か(関数名に使用される)
+/// @param _storageName ストレージの変数名
+/// @param _memberTag ストレージ内の公開したい変数のタグ
 ///
 /// @warning 取得した参照やRangeは再確保後に無効になります。
 /// @warning 対応したStorageじゃない場合エラーになるときがあります。
-#define MD_OWNED_STORAGE_READ_WRITE_ACCESSORS(IDType, MemberType, _name, _storageName)\
+#define MD_OWNED_STORAGE_READ_WRITE_ACCESSORS(IDType, MemberType, _tag, _storageName, _memberTag)\
 public:\
 	/* 値読み取り */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_name(IDType _id) const\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_tag(IDType _id) const\
 	{\
-		return _storageName->Get##_name(GetDenseIndex(_id));\
+		return _storageName->Get##_memberTag(GetDenseIndex(_id));\
 	}\
 	/* 値参照渡しで変更できる */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Edit##_name(IDType _id)\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Edit##_tag(IDType _id)\
 	{\
-		return _storageName->Edit##_name(GetDenseIndex(_id));\
+		return _storageName->Edit##_memberTag(GetDenseIndex(_id));\
 	}\
 	/* 値代入 */\
-	void Set##_name(IDType _id, const MemberType& _value)\
+	void Set##_tag(IDType _id, const MemberType& _value)\
 	{\
-		_storageName->Set##_name(GetDenseIndex(_id), _value);\
+		_storageName->Set##_memberTag(GetDenseIndex(_id), _value);\
 	}\
 	/* 配列の要素数 */\
-	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_tag() const noexcept\
 	{\
-		return _storageName->Count##_name();\
+		return _storageName->Count##_memberTag();\
 	}\
 	/* 空チェック */\
-	[[nodiscard("戻り値が無視されています")]] bool Empty##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] bool Empty##_tag() const noexcept\
 	{\
-		return _storageName->Empty##_name();\
+		return _storageName->Empty##_memberTag();\
 	}\
 	/* 変化不可配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Get##_name##Range() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Get##_tag##Range() const noexcept\
 	{\
-		return _storageName->Get##_name##Range();\
+		return _storageName->Get##_memberTag##Range();\
 	}\
 	/* 変化可能配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Edit##_name##Range() noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Edit##_tag##Range() noexcept\
 	{\
-		return _storageName->Edit##_name##Range();\
+		return _storageName->Edit##_memberTag##Range();\
 	}
 
-/// @def MD_OWNED_STORAGE_READ_WRITE_ACCESSORS(IDType, _name, _storageName, _member)
+/// @def MD_OWNED_STORAGE_READ_WRITE_ACCESSORS(IDType, _tag, _storageName, _member, _memberTag)
 /// @brief 読み込み可能な子ストレージのラッパー関数を用意します。
 ///
 /// 次の関数を生成します。
@@ -351,35 +352,36 @@ public:\
 ///
 /// @param IDType 対応するIDの型
 /// @param MemberType 変数の型
-/// @param _name 関数名に使用する名前
-/// @param _storageName ストレージの変数
+/// @param _tag 変数が何か(関数名に使用される)
+/// @param _storageName ストレージの変数名
+/// @param _memberTag ストレージ内の公開したい変数のタグ
 ///
 /// @warning 取得した参照やRangeは再確保後に無効になります。
 /// @warning 対応したStorageじゃない場合エラーになるときがあります。
-#define MD_OWNED_STORAGE_READ_ONLY_ACCESSORS(IDType, MemberType, _name, _storageName)\
+#define MD_OWNED_STORAGE_READ_ONLY_ACCESSORS(IDType, MemberType, _tag, _storageName, _memberTag)\
 public:\
 	/* 値読み取り */\
-	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_name(IDType _id) const\
+	[[nodiscard("戻り値が無視されています")]] decltype(auto) Get##_tag(IDType _id) const\
 	{\
-		return _storageName->Get##_name(GetDenseIndex(_id));\
+		return _storageName->Get##_memberTag(GetDenseIndex(_id));\
 	}\
 	/* 配列の要素数 */\
-	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] uint32_t Count##_tag() const noexcept\
 	{\
-		return _storageName->Count##_name();\
+		return _storageName->Count##_memberTag();\
 	}\
 	/* 空チェック */\
-	[[nodiscard("戻り値が無視されています")]] bool Empty##_name() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] bool Empty##_tag() const noexcept\
 	{\
-		return _storageName->Empty##_name();\
+		return _storageName->Empty##_memberTag();\
 	}\
 	/* 変化不可配列取得(全探査用) */\
-	[[nodiscard("戻り値が無視されています")]] auto Get##_name##Range() const noexcept\
+	[[nodiscard("戻り値が無視されています")]] auto Get##_tag##Range() const noexcept\
 	{\
-		return _storageName->Get##_name##Range();\
+		return _storageName->Get##_memberTag##Range();\
 	}\
 
-/// @def MD_OWNED_STORAGE_WRITE_ORIGINAL_ACCESSORS(IDType, MemberType, _name, _storageName)
+/// @def MD_OWNED_STORAGE_WRITE_ORIGINAL_ACCESSORS(IDType, MemberType, _tag, _storageName, _memberTag)
 /// @brief 独自にSetterを作成可能な子ストレージのラッパー関数を用意します。
 ///
 /// 次の関数を生成します。
@@ -390,10 +392,11 @@ public:\
 ///
 /// @param IDType 対応するIDの型
 /// @param MemberType 変数の型
-/// @param _name 関数名に使用する名前
-/// @param _storageName ストレージの変数
+/// @param _tag 変数が何か(関数名に使用される)
+/// @param _storageName ストレージの変数名
+/// @param _memberTag ストレージ内の公開したい変数のタグ
 ///
 /// @warning 取得した参照やRangeは再確保後に無効になります。
 /// @warning 対応したStorageじゃない場合エラーになるときがあります。
-#define MD_OWNED_STORAGE_WRITE_ORIGINAL_ACCESSORS(IDType, MemberType, _name, _storageName)\
-	MD_OWNED_STORAGE_READ_ONLY_ACCESSORS(IDType, MemberType, _name, _storageName);
+#define MD_OWNED_STORAGE_WRITE_ORIGINAL_ACCESSORS(IDType, MemberType, _tag, _storageName, _memberTag)\
+	MD_OWNED_STORAGE_READ_ONLY_ACCESSORS(IDType, MemberType, _tag, _storageName, _memberTag);
