28d8f7519856ee8b1caf2cd00cfcfdb41053a906
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-28 09:46:30 +0900

    PageTable追加

--- SoftBodyProject/Source/Common/ECS/SparseSet/Table/SparseSetTableInterface.h
+++ SoftBodyProject/Source/Common/ECS/SparseSet/Table/SparseSetTableInterface.h
@@ -12,7 +12,7 @@ public:
 	virtual void Reserve(uint32_t _size) = 0;
 	virtual std::optional<uint32_t> Find(uint32_t _keyIndex) const = 0;
 	virtual void Set(uint32_t _keyIndex, uint32_t _denseIndex) = 0;
-	virtual void Erase(uint32_t _key) = 0;
+	virtual void Erase(uint32_t _keyIndex) = 0;
 	virtual void Clear() = 0;
 
 	virtual ~SparseSetTableInterface() = default;
