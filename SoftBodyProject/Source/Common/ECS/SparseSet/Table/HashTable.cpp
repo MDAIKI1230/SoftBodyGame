4357ab10d4bad5e34c0d3d5b1c41e4c79aa64f71
MDAIKI1230 <shimamizu1230@gmail.com>
2026-07-28 09:56:02 +0900

    HashTable追加

---
+++ SoftBodyProject/Source/Common/ECS/SparseSet/Table/HashTable.cpp
@@ -0,0 +1,33 @@
+﻿#include "HashTable.h"
+
+void HashTable::Reserve(uint32_t _size)
+{
+    slots.reserve(_size);
+}
+
+std::optional<uint32_t> HashTable::Find(uint32_t _keyIndex) const
+{
+    const auto it = slots.find(_keyIndex);
+
+    if (it == slots.end())
+    {
+        return std::nullopt;
+    }
+
+    return it->second;
+}
+
+void HashTable::Set(uint32_t _keyIndex, uint32_t _denseIndex)
+{
+    slots.insert_or_assign(_keyIndex, _denseIndex);
+}
+
+void HashTable::Erase(uint32_t _keyIndex)
+{
+    slots.erase(_keyIndex);
+}
+
+void HashTable::Clear()
+{
+    slots.clear();
+}
---
+++ SoftBodyProject/Source/Common/ECS/SparseSet/Table/HashTable.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <unordered_map>
+
+#include "SparseSetTableInterface.h"
+
+class HashTable : public SparseSetTableInterface
+{
+public:
+    // メモリ確保
+    void Reserve(uint32_t _size) override;
+    // 探す
+    std::optional<uint32_t> Find(uint32_t _keyIndex) const override;
+    // 新たに追加
+    void Set(uint32_t _keyIndex, uint32_t _denseIndex) override;
+    // 削除
+    void Erase(uint32_t _keyIndex) override;
+    // 全要素なくす。
+    void Clear() override;
+
+private:
+    std::unordered_map<uint32_t, uint32_t> slots;
+};
