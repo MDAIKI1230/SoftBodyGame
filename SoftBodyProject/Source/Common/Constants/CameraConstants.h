2e50fcc165c112c300d289d8424abd624f540127
MDAIKI1230 <shimamizu1230@gmail.com>
2026-08-19 20:12:51 +0900

    カメラComponentの用意

---
+++ SoftBodyProject/Source/Common/Constants/CameraConstants.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+enum class ProjectionType
+{
+	PERSPECTIVE,
+	ORTHOGRAPHIC
+};
+
+enum class ClearMode
+{
+	SOLID_COLOR,
+	SKY
+};
---
+++ SoftBodyProject/Source/System/Components/CameraComponent.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CameraConstants.h"
+
+#include "ViewPort.h"
+#include "Color.h"
+
+struct CameraComponent
+{
+private:
+	// カメラ描画距離
+	float near{ 0.1f };
+	float far{ 1000.0f };
+
+	// FOV(度数法)
+	float fov{ 60.0f };
+
+	// 優先度
+	int priority{ 0 };
+
+	// 生存フラグ
+	bool isActive{ true };
+
+	// 投影方式
+	ProjectionType projectionType{ ProjectionType::PERSPECTIVE };
+
+	// 画面の消去方式
+	ClearMode clearMode{ ClearMode::SOLID_COLOR };
+
+	// SolidModeの時の色
+	Color solidColor{ 255,255,255 };
+
+	// 描画範囲矩形
+	ViewPort viewPort;
+};
