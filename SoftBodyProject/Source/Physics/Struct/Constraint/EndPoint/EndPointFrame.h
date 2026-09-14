01e75a834b6f468f5c17b1ef86753eac0437926d
MDAIKI1230 <shimamizu1230@gmail.com>
2026-09-14 12:09:16 +0900

    EndPointFrame追加

---
+++ SoftBodyProject/Source/Physics/Struct/Constraint/EndPoint/EndPointFrame.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "MDMath.h"
+
+#include "PhysicsTransformID.h"
+
+/*
+	拘束に使う位置や姿勢の情報構造体
+*/
+struct EndPointFrame
+{
+	PhysicsTransformID transformID;
+
+	// 拘束点のローカル位置
+	Vector3 localPosition;
+	// 拘束点のローカル姿勢
+	Quaternion localRotation;
+};
